39302152931c243b53dbe39524a2ee622be653a2
solmin <solminkim03@naver.com>
2021-05-28 19:01:18 +0900

    21.05.28 _ 수정

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import './use-state-demo/counter-with-hook'
+// import './use-state-demo/counter-with-hook'
+// import './use-state-demo/counter-with-hook-caveat'
+// import './use-state-demo/use-state-object-manage-caveat'
+//  import './use-state-demo/theme-component-demo'
+//  import './use-state-demo/todo-app-demo'
+ import './use-effect-demo/use-effect-deps'
