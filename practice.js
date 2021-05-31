e0fd76f95e598f189cca1d93ce1126aa3df56743
solmin <solminkim03@naver.com>
2021-05-31 21:15:10 +0900

    21.05.31 _ useEffect

---
+++ practice.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from "react"
+import ReactDOM from "react-dom"
+
+const [state,setState] = useState("")
+
+function Counter(props){
+    const[count, setCount]=useState(0);
+}
+
+/*
+    return {...prev,count:prev.count+1 }
+*/
