eb877ec235f9b1f89a57d6c6246438637a574ea1
solmin <solminkim03@naver.com>
2021-06-06 11:52:05 +0900

    수정

--- src/index.js
+++ src/index.js
@@ -3,4 +3,10 @@
 // import './use-state-demo/use-state-object-manage-caveat'
 //  import './use-state-demo/theme-component-demo'
 //  import './use-state-demo/todo-app-demo'
- import './use-effect-demo/use-effect-deps'
+
+
+//  import './use-effect-demo/use-effect-deps'
+//  import './use-effect-demo/use-effect-unmount-deps'
+//  import './use-effect-demo/use-effect-with-dom'
+//  import './use-effect-demo/use-effect-unmount'
+ import './use-effect-demo/use-effect-fetch-api'
